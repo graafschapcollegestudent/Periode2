45a904f2fce412da2c51b6fa4f9613eae9db7108
Hadi <167310042+graafschapcollegestudent@users.noreply.github.com>
2026-06-12 13:07:49 +0200

    Test CI pipeline

--- tasktracker-api/routes/api.php
+++ tasktracker-api/routes/api.php
@@ -8,3 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/tasks', [TaskController::class, 'index']);
 
 Route::post('/tasks', [TaskController::class, 'store']);
+
+Route::get('/task/complete', [TaskController::class, 'completed']);
+Route::put('/task/{task}/complete', [TaskController::class, 'complete']);
+Route::put('/task/{task}', [TaskController::class, 'update']);
+Route::delete('/task/{task}', [TaskController::class, 'destroy']);
